3e0818a5fe44627041e5aafc273743842be7e3c7
virajransing <virajransing@gmail.com>
2019-08-21 21:00:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,58 @@
+def using_push(array,string)
+  array.push(string)
+end 
+
+def using_unshift(array,string)
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  array.pop()
+end 
+
+def pop_with_args(array)
+  array.pop(2)
+end 
+
+def using_shift(array)
+  arry.shift()
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array_1,array_2)
+  array_1.conat(array_2)
+end 
+
+def using_insert(array,element)
+  array.insert(4,element)
+end 
+
+def uniq(array)
+  array.uniq()
+end 
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+  
+def using_delete_at(array, interger)
+  array.delete(integer)
+end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
